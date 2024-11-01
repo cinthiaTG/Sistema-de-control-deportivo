ac4308e287f41738cf546b65aa860b64105a757a
cinthiaTG <2230282@upv.edu-mx>
2024-11-01 12:38:28 -0500

    primeros cambios de separacion

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.plantilla')
+
+@section('content')
+
+<h2>Pagina inicial</h2>
+    <!--
+        la plantilla base esta en layouts aqui es como una seccion la cual la puedes modificar segun te convenga 
+        solo puse un ejemplo pero igual ocupamos crear estilos que nos funcionen a cada uno
+        
+    -->
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+include base_path('routes/routes_cat_s.php');
+include base_path('routes/routes_cat_c.php');
